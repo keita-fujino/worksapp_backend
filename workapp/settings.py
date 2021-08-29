e8a0aab09bb1fb14e7295a189cf25f88f493b18b
keita <keitaf.0344@gmail.com>
2021-08-29 22:55:19 +0900

    allowed host added

--- workapp/settings.py
+++ workapp/settings.py
@@ -30,7 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'worksapp-backend.herokuapp.com'
+]
 
 
 # Application definition
